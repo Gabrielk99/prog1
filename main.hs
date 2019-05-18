186879db98582af6ab0f94c43be47ac6683b0f55
Gabrielk99 <34044829+Gabrielk99@users.noreply.github.com>
2019-05-18 14:22:14 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,5 +1,7 @@
 import Text.Printf
 import System.IO
+import Data.List
+
 main = do 
     k'<- readFile "k.txt"
     dt<- readFile "entrada.txt"
@@ -7,78 +9,75 @@ main = do
     let aux1 = map (words) aux 
     let pontos = [map (read::String->Double) xs|xs<-aux1]
     let k = (read k' ::Int)
-    let grupos = kGruposForm k pontos
-    let saida = gerarSaida pontos k 0
-    gerandoSaida saida
-    gerandoSaida grupos 
-    writeFile "result.txt" (printaSSE pontos k 0)
+    let grupos = startKmeans pontos k 0
+    let indices = gerarSaida' pontos grupos
+    let saida = gerandoSaida indices
+    let sSE = _SSE grupos
+    writeFile "saida.txt" saida 
+    writeFile "result.txt" (printaSSE sSE)
     
 
+----------------------- FUNÇÕES AUXILIARES PARA SALVAR A SAIDA -------------------------- 
+--printaSSE retorna a soma SSE formatada para a precisão das 4 casas decimais--
+printaSSE ::Double->String
+printaSSE sSE = printf "%.4f" sSE
 
+--transforma os indices que precisam ser gerados em uma string-- 
+gerandoSaida :: Show a => [[a]] -> [Char]
+gerandoSaida [] = []
+gerandoSaida (xs:xss) = ( concat(intersperse ", " saida' ) ++"\n\n")++gerandoSaida xss
+                    where 
+                        saida' = map show xs
+----------------------------------------------------------------------------------------
 
--- FUNÇÕES AUXILIARES PARA SALVAR A SAIDA -- 
-printaSSE ::[[Double]] -> Int -> Int ->String
-printaSSE pontos k lim = printf "%.4f" (_SSE pontos k lim)
+------------ FUNÇÕES AUXILIARES PARA ORDENAR COM O SORTBY ------------- 
+ordena'::Ord a=> a->a->Ordering
+ordena' ys xs |ys<xs = LT
+              |otherwise = GT
+ordena::(Foldable t,Num a,Ord a,Ord(t a))=> t a->t a->Ordering
+ordena ys xs |sum ys < sum xs = LT
+             |sum ys == sum xs = ordena' ys xs
+             |otherwise = GT
+ordenaD'::Ord a=> a->a->Ordering
+ordenaD' ys xs |ys<xs = LT
+               |otherwise = GT
+ordenaD :: [Double]->[Double]->[Double]->Ordering
+ordenaD ct ys xs|distEuc ys ct > distEuc xs ct = LT
+                |distEuc ys ct == distEuc xs ct = ordenaD' ys xs
+                |otherwise = GT
+ordenaM' :: Ord a=>a->a->Ordering
+ordenaM' ct1 ct2 | ct1<ct2=LT
+                 | otherwise=GT
+ordenaM :: [Double]->[Double]->[Double]->Ordering
+ordenaM pt ct1 ct2|distEuc ct1 pt < distEuc ct2 pt = LT
+                  |distEuc ct1 pt == distEuc ct2 pt = ordenaM' ct1 ct2
+                  |otherwise = GT 
+--------------------------------------------------------------------------
 
+{---------------------------------------------------- COMEÇANDO O ALGORITIMO -----------------------------------------------------------------}
 
-geraList [] = []
-geraList (x:xs) | x>0 = [x]++geraList xs
-                |otherwise = geraList xs
-geraList' [] = []
-geraList' (xs:xss) = [geraList xs] ++ geraList' xss 
+--Ordeno os pontos com sortBy utilizando como parametro ordena, onde ordena é uma função que compara as somas das coordenadas dos pontos--
+--Crescentemente de acordo com a soma, dessa forma head xss retorna o ponto de menor soma das coordenadas--
+menorSomaCord :: (Foldable t,Num a,Ord a, Ord (t a))=>[t a]->t a
+menorSomaCord xss = head (sortBy ordena xss)
 
-gerandoSaida [] = return ()
-gerandoSaida (xs:xss) = do
-                let saida1 = map (show) xs
-                let saida'=map (++ ", ") saida1
-                let saida = unwords saida'
-                appendFile "saida.txt" (saida ++ "\n\n")
-                gerandoSaida xss
-
---MERGE SORT PARA ORDENAR OS PONTOS-- 
-mergeSort :: Ord a => [a] -> [a]
-mergeSort [] = []
-mergeSort [x]=[x]
-mergeSort xss =  merge (mergeSort (take ((length xss) `div`2) xss)) (mergeSort (drop ((length xss) `div` 2) xss))
-
-merge:: Ord a=> [a]->[a]->[a]
-merge [] ys = ys
-merge xs [] = xs
-merge (xs:xss) (ys:yss) | xs<ys = [xs] ++ merge xss (ys:yss)
-                    | otherwise = [ys] ++ merge (xs:xss) yss 
-
---PEGANDO O PONTO DE MENOR SOMA DAS CORDENADAS--
--- a ideia é ordenar crescentemente garantindo que o ponto de menor soma das cordenadas seja o primeiro -- 
-menorSomaCord :: Ord a=> [a]-> a
-menorSomaCord xss = head (mergeSort xss)
-
---DISTANCIA EUCLIDEANA--
+-------------------DISTANCIA EUCLIDEANA----------------------
 listdistEuc :: [Double]->[Double]->[Double]
 listdistEuc [] [] = []
 listdistEuc (x:xs) (y:ys) = [(x-y)^2] ++ listdistEuc xs ys
 
 distEuc :: [Double]->[Double]->Double
 distEuc xs ys = sqrt (sum (listdistEuc xs ys))
+-------------------------------------------------------------
 
--- ordeno a lista e pego o ultimo, esse me garante que é a maior distancia possível do ponto que quero comparar --
-maiordistEuc :: [[Double]] -> [Double] -> Double
-maiordistEuc xss ys = distEuc (last(mergeSort xss)) ys
+-- Ordeno os pontos a partir da distancia de forma que os que tiverem maior distancia ao ponto ys fique no começo --
+--quando os pontos possuem a mesma distancia ao ponto ys, avalio o que tem as menores coordenadas no começo -- 
+maiordistEuc :: [[Double]] -> [Double] -> [Double]
+maiordistEuc xss ys =  head(sortBy (ordenaD ys) xss)
 
--- Aqui vamos receber a lista ordenada de forma decrescente, 
--- sabemos que o ponto mais distante ao centroide será o ponto com as maiores coordenadas
--- Por que fazer isso? Quando encontrarmos um que já é diferente da maior distancia quer dizer que não tem mais nenhum
-pontosMaisDist :: [[Double]]->Double->[Double]->[[Double]]
-pontosMaisDist [] maiorD ys = []
-pontosMaisDist (xs:xss) maiorD ys | maiorD == distEuc xs ys = [xs]++ pontosMaisDist xss maiorD ys
-                                  | otherwise = []
-
--- pego os pontos mais distantes tendo em consideração que estou mandando em ordem decrescente
--- ordeno os pontos que recebi e retiro a cabeça, como ta em ordem crescente esse é com certeza o ponto com maior dist
--- e menores primeiras cord
-pontoMaisDistMenorCord ::  [[Double]]->[Double]->[Double]                           
-pontoMaisDistMenorCord xss ys = head(mergeSort(pontosMaisDist  (reverse (mergeSort xss)) (maiordistEuc xss ys) ys))
+---------------------------------------------------- CALCULO DO CENTROIDE-----------------------------------------------------------------
 --Aqui estou achando nosso centroide
---centroide' é responsável por somar cordenadas por cordenadas
+--centroide' é responsável por somar coordenadas por coordenadas
 --centroide'' é por mandar sempre um ponto estatico e uma lista de pontos, dai 
 --vamos sempre chamar centroide'' sendo xs trocado pelo centroide' de xs e ys
 --centroide3 é a função que vai chamar centroide'' dizendo que o ponto estatico é a cabeça e a lista de pontos é a cauda
@@ -100,8 +99,9 @@ centroide4 xss = [x/(fromIntegral tam)| x<-xs]
                 where xs = centroide3 xss
                       tam = length xss
 
+--------------------------------------------------------------------------------------------------------------------------------------------
 
---MONTANDO OS K CENTROIDES--
+---------------------------------------MONTANDO OS K CENTROIDES---------------------------------------------
 
 --verifico se k é maior que 0, caso seja chamo a função novamente
 -- com o novo valor de k = k-1 e mando a lista de pontos com menos o ponto mais distante do centroide 
@@ -111,11 +111,11 @@ iniciarGrupos k [] yss = yss
 iniciarGrupos k xss yss | k > 0 = iniciarGrupos (k-1) (removeEle ptMDist xss ) (yss++[ptMDist])
                         | otherwise = yss
                         where 
-                            ptMDist = pontoMaisDistMenorCord xss (centroide4 yss)
+                            ptMDist = maiordistEuc xss (centroide4 yss)
         
 -- função geradora dos k centroides
---chamo a função auxiliar com k-2 pois os dois primeiros pontos ja estão sendo passados
---e mando a lista atualizada com menos os dois primeiros pontos
+--chamo a função auxiliar com k-1 pois o  primeiro ponto ja está sendo passado
+--e mando a lista atualizada com menos um ponto
 -- K tem que ser necessáriamente maior que zero, se não retorna lista vazia
 kGruposForm :: (Ord t, Num t) => t->[[Double]] -> [[Double]]
 kGruposForm 0 _ = []
@@ -128,31 +128,17 @@ removeEle :: Eq t => t->[t]->[t]
 removeEle ys [] = [] 
 removeEle ys (xs:xss) | ys == xs = xss
                       | otherwise = [xs]++removeEle ys xss
+-------------------------------------------------------------------------------------------------------------
 
---CRIANDO OS GRUPOS KMEANS--
 
---Acha a menor distancia do ponto aos centroides--
-minDistCent::[Double]->Double->[[Double]]->Double
-minDistCent _ min [] = min
-minDistCent xs min (ys:yss) | min > dist = minDistCent xs dist yss
-                            | otherwise  = minDistCent xs min yss
-                            where 
-                                dist = distEuc xs ys
-
---mandar os centroides ordenados em ordem crescente--
---como os centroides tão em ordem crescente o primeiro que tiver a dist igual a minima é o centroide 
---de menores primeiras cordenadas
-acharPtdeMenordist ::  [Double]->Double ->[[Double]]->[Double]
-acharPtdeMenordist _ _ [] = []
-acharPtdeMenordist xs min (ys:yss)| distEuc xs ys == min = ys 
-                                  | otherwise = acharPtdeMenordist xs min yss
+---------------------------------------------CRIANDO OS GRUPOS KMEANS--------------------------------------------------------
 
 --Crio os grupos a partir dos centroides e tuplas
 --onde as tuplas são (chave,conteudo)
 --em que a chave é a que centroide o conteudo pertence
 criandoOsGrupos :: Eq a=> [(a,a)]->[a]->[[a]]
-criandoOsGrupos xss [] = []
-criandoOsGrupos xss (cs:centroides) = [[cs]++[snd xs| xs<-xss, (fst xs) == cs]] ++ criandoOsGrupos xss centroides
+criandoOsGrupos tuplas [] = []
+criandoOsGrupos tuplas (cs:centroides) = [[cs]++[snd tp| tp<-tuplas, (fst tp) == cs]] ++ criandoOsGrupos tuplas centroides
 
 --Aqui ocorre a atualização dos centroides--
 newCentroides :: [[[Double]]]->[[Double]]
@@ -164,23 +150,23 @@ newCentroides grupos = [centroide4 xss | xss<-grupos ]
 --dai crio uma lista dessas tuplas
 comparePtCent :: [[Double]]->[[Double]]->[([Double],[Double])]
 comparePtCent [] _= []
-comparePtCent (xs:xss) centroides = [(chave,xs)]++comparePtCent xss centroides
+comparePtCent (pt:pontos) centroides = [(chave,pt)]++comparePtCent pontos centroides
                                 where 
-                                    min = minDistCent xs (distEuc xs (head centroides)) (tail centroides)
-                                    chave = acharPtdeMenordist xs min (mergeSort centroides)
+                                    chave = head(sortBy (ordenaM pt) centroides)
 
 --Aqui é onde a magica acontece, as operações anteriores de criar tuplas, separar os grupos e recalcular centroides--
 --como estabelecido, comparamos se houve mudanças no grupo e se o lim é menor igual a 100
 sepOsGrupos :: (Ord t1,Num t1)=>[[Double]]->t1->[[Double]]->[[[Double]]]->[[[Double]]]
 sepOsGrupos xss lim centroidesAtt gruposAnt | lim <=100 && grupos /= gruposAnt = sepOsGrupos xss (lim+1) centroidesN grupos
                                             | otherwise = grupos
-                                    where
+
+                                            where
                                         centroidesN = newCentroides grupos
                                         grupos = criandoOsGrupos listDeTuplas centroidesAtt
                                         listDeTuplas = comparePtCent xss centroidesAtt
 
 --Função para chamar a sepOsGrupos, auxiliar para ter um primeiro grupo de partida para poder comparar
---Ela pega o primeirou grupo, calcula os novos centroides e manda para a função sepOsGrupos
+--Ela pega o primeiro grupo, calcula os novos centroides e manda para a função sepOsGrupos
 startKmeans :: (Ord t2,Ord t3,Num t2,Num t3)=>[[Double]]->t3->t2->[[[Double]]]
 startKmeans xss k lim  = sepOsGrupos xss (lim+1) centroidesN grupos
                         where 
@@ -189,43 +175,42 @@ startKmeans xss k lim  = sepOsGrupos xss (lim+1) centroidesN grupos
                             listDeTuplas = comparePtCent xss centroides
                             centroides = kGruposForm k xss
 
---CALCULA O SSE--
-_SSE :: (Ord t1,Ord t2,Num t1,Num t2)=> [[Double]]->t2->t1->Double
-_SSE xss k lim = sum[(distEuc xs (centroide4 xss))^2|xss<-grupos,xs<-xss]
-    where 
-        grupos = startKmeans xss k lim
+---------------------------------------------------------------------------------------------------------------------------------
 
---gero tuplas com indices onde cada ponto é representado por um indice--
-geraTuplasIndice :: Num t=> t-> [b] -> [(t,b)]
-geraTuplasIndice  _ [] = []
-geraTuplasIndice indice (xs:xss) = [(indice,xs)]++geraTuplasIndice (indice+1) xss
+---------------------------------------------------CALCULA O SSE----------------------------------------------------------
+_SSE ::[[[Double]]]->Double
+_SSE grupos = sum[(distEuc xs (centroide4 xss))^2|xss<-grupos,xs<-xss]
+--------------------------------------------------------------------------------------------------------------------------
+
+
+--------------------------------------------GERANDO LISTA DE INDICES---------------------------------------------------------
 
 --retorna o indice que um ponto do grupo é representado pelos indices dos pontos iniciais--
-retornaIndice :: (Num p, Eq t)=> t->[(p,t)]->p
-retornaIndice xs []  = -1
-retornaIndice xs (tp:tuplas)| xs == snd tp = fst tp
-                            | otherwise = retornaIndice xs tuplas
+retornaIndice :: (Eq t1,Num t2)=>t1->[t1]->t2->t2
+retornaIndice xs (ys:yss) id| xs == ys = id
+                            | otherwise = retornaIndice xs yss (id+1)
+                           
 
-removeTup id (tp:tps) | id == fst tp = tps
-                      | otherwise = [tp]++removeTup id tps
+--verifica o ponto que ja foi indexado e o configura como uma lista vazia para que na proxima chamada
+--esse mesmo ponto não seja localizado nesse indixe ja utilizado
+atualizaPts :: Eq a => [[a]]->[a]->[[a]]   
+atualizaPts [] [] = []
+atualizaPts xss [] = xss
+atualizaPts (xs:xss) gs | xs == gs = [[]] ++ xss
+                        | otherwise = [xs]++atualizaPts xss gs
+
 --retorno uma lista de indices a partir de um conjunto de pontos, 
---verifico se o ponto ja foi indexado, pois é possível que o centroide final permaneça igual a um ponto inicial
-gerarSaida'' :: (Num a1, Eq a2,Eq a1)=>[(a1,a2)]->[a2]->[a1]
-gerarSaida'' tuplas [] = []
-gerarSaida'' tuplas (gs:gss) = [id] ++ gerarSaida'' newTup gss
+--Atualizo os pontos antes de gerar um novo indice, pois pode haver pontos repetidos em indices diferentes--
+gerarSaida'' :: (Eq a1,Num a2)=>[[a1]]->[[a1]]->[a2]
+gerarSaida'' xss [] = []
+gerarSaida'' xss (gs:gss) =  [id] ++ gerarSaida'' pontosAtt gss
                             where 
-                                id = retornaIndice gs tuplas 
-                                newTup = removeTup id tuplas
---gera uma lista de lista dos indices--
-gerarSaida' :: (Eq a2,Eq a1,Num a1)=>[a2]->[[a2]]->[[a1]]
-gerarSaida' xss [] = []
-gerarSaida' xss (gss:grupos) = [gerarSaida'' tuplas gss]++gerarSaida' xss grupos
-                    where 
-                        tuplas = geraTuplasIndice 1 xss
+                                id = retornaIndice gs xss 1
+                                pontosAtt = atualizaPts xss gs
 
---retorna todo o processo feito por gerarSaida' e gerarSaida''--
-gerarSaida :: (Ord a,Ord t2,Ord t3,Num t2,Num t3,Num a)=> [[Double]]->t3->t2->[[a]]
-gerarSaida xss k lim = geraList' saida
-                    where 
-                        grupos = startKmeans xss k lim
-                        saida = gerarSaida' xss grupos
+--gera uma lista de lista dos indices--
+gerarSaida' :: (Eq a1,Num a2)=>[[a1]]->[[[a1]]]->[[a2]]
+gerarSaida' xss [] = []
+gerarSaida' xss (gss:grupos) = [gerarSaida'' xss (tail gss)]++gerarSaida' xss grupos
+
+--------------------------------------------------------------------------------------------------------------------------
